65d82a8becf18f27c9b5810cfb4a67b1b99344c0
Blazej Gracz <gracz.blazej@gmail.com>
2020-11-07 23:39:00 +0100

    add config file

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+consumer_key:'Vw5wjlBHNexv7R9rwmdu7VGfR',
+consumer_secret: 'R2davneuwFrMWeEL3iKan439du0h12GqpXneCLGzuuGcS0QuqW',
+access_token: '1173154760620498944-IHvhojZOrGDqcB5yIHXsRMTi74Qt7Z',
+access_token_secret: '3pd0MYtONja3MDRnUr35LY3BfybOBkWI8mkyAwa5Heta7'
+}
