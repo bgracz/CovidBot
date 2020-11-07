a52200ab99c94e687144c4796cc857a3755a0e37
Blazej Gracz <gracz.blazej@gmail.com>
2020-11-07 23:26:56 +0100

    Ready?

--- bot.js
+++ bot.js
@@ -1,20 +1,42 @@
 var Twit = require('twit');
 var config = require('./config.js');
+var fs = require('fs');
+const fetch = require("node-fetch");
 var T = new Twit(config);
+
+setInterval(tweeter, 60*60*3*1000); //60*60 to godzina
 
 tweeter();
 
-setInterval(tweeter, 60*5*1000);
-
 function tweeter() {
-  var tweet = 'Random for today ' + Math.random()*100;
-  T.post('statuses/update', {status: tweet }, tweeted);
 
-    function tweeted(err, data, response) {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('Success:' + data.text);
-      }
-    };
+  fetch('https://api.covid19api.com/summary')
+  .then(response => {
+    return response.json();
+  })
+  .then(users => {
+    // console.log(users);
+    var json = JSON.stringify(users, null, 2);   //punkt pierwszy przy czytaniu JSON
+    fs.writeFileSync('tweets.json', json);      //towrzy plik json
+
+    var obj = JSON.parse(json);     //punkt drugi przy czytaniu JSON
+    var globalNewConfirmed = obj.Global.NewConfirmed;   //zmienna "b" jako odnośnik do Global -> NewConfirmed w JSON
+
+    //polska
+    var namePoland = obj.Countries[135].Country;
+    var laczniePrzypadkow = obj.Countries[135].TotalConfirmed.toLocaleString();
+    var nowychPrzypadkow = obj.Countries[135].NewConfirmed.toLocaleString();
+    var lacznieZmarlo = obj.Countries[135].TotalDeaths.toLocaleString();
+    var lacznieWyzdrowialo = obj.Countries[135].TotalRecovered.toLocaleString();
+
+    var dataForTweet = ("Wszystkich przypadkow " + laczniePrzypadkow + " w tym nowych " + nowychPrzypadkow + ". Od poczatku pandemii zmarlo " + lacznieZmarlo + " chorych, a wyzdrowialo " + lacznieWyzdrowialo + " przypadkow. #covid19 #stopcovid #sarscov2 #koronawirus");
+    // console.log(dataForTweet);
+
+    T.post('statuses/update', { status: dataForTweet }, function(err, data, response) {
+    console.log(data)
+})
+  });
+
+
+
 };
