5203748282fae07fa44ac93333db692fe5712182
Blazej Gracz <gracz.blazej@gmail.com>
2020-11-09 19:50:38 +0100

    New version bot, all working

--- bot.js
+++ bot.js
@@ -1,13 +1,14 @@
 const PORT = process.env.PORT || 5001;
 var express = require('express');
-var app = express();
+var app = express();   //wszystkie trzy wymagane dal heroku
+
 var Twit = require('twit');
 var config = require('./config.js');
-var fs = require('fs');
+//var fs = require('fs');
 const fetch = require("node-fetch");
 var T = new Twit(config);
 
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`)); //dla heroku
 
 setInterval(tweeter, 60*29*1000); //60*60 to godzina
 
@@ -22,7 +23,7 @@ function tweeter() {
   .then(users => {
     // console.log(users);
     var json = JSON.stringify(users, null, 2);   //punkt pierwszy przy czytaniu JSON
-    fs.writeFileSync('tweets.json', json);      //towrzy plik json
+    //fs.writeFileSync('tweets.json', json);      //towrzy plik json
 
     var obj = JSON.parse(json);     //punkt drugi przy czytaniu JSON
     var globalNewConfirmed = obj.Global.NewConfirmed;   //zmienna "b" jako odnośnik do Global -> NewConfirmed w JSON
@@ -33,9 +34,12 @@ function tweeter() {
     var nowychPrzypadkow = obj.Countries[135].NewConfirmed.toLocaleString();
     var lacznieZmarlo = obj.Countries[135].TotalDeaths.toLocaleString();
     var lacznieWyzdrowialo = obj.Countries[135].TotalRecovered.toLocaleString();
+    var nowychWyzdrowialo = obj.Countries[135].NewRecovered.toLocaleString();
+    var nowychZmarlo = obj.Countries[135].NewDeaths.toLocaleString();
+    var ostatniaAktualizacja = obj.Countries[135].Date;
 
-    var dataForTweet = ("Wszystkich przypadkow " + laczniePrzypadkow + " w tym nowych " + nowychPrzypadkow + ". Od poczatku pandemii zmarlo " + lacznieZmarlo + " chorych, a wyzdrowialo " + lacznieWyzdrowialo + " przypadkow. #covid19 #stopcovid #sarscov2 #koronawirus");
-    // console.log(dataForTweet);
+    var dataForTweet = ("Chorych: " + laczniePrzypadkow + " (+ " + nowychPrzypadkow + "). \nWyzdrowialo: " + lacznieWyzdrowialo + " (+ " + nowychWyzdrowialo +"). \nZmarlo: " + lacznieZmarlo + " (+ " + nowychZmarlo + "). \n\nDane z " + ostatniaAktualizacja.substring(0,10) + " " + ostatniaAktualizacja.substring(11,16) + "\n#covid19 #sarscov2");
+    //console.log(dataForTweet);
 
     T.post('statuses/update', { status: dataForTweet }, function(err, data, response) {
     console.log(data)
