4455fcd027fca5e92a8a308b89c7ef33112286f7
Blazej Gracz <gracz.blazej@gmail.com>
2021-04-19 22:59:09 +0200

    cleaning in code

---
+++ README.md
@@ -0,0 +1,4 @@
+Covid Bot
+
+This is a twitter bot that uses the API to download data. The program collects data on current disease statistics and then searches for an object corresponding to the country (deafault it's Poland).
+Finally, the bot creates a tweet and publishes the statistics on the website. Code that runs every hour.
--- bot.js
+++ bot.js
@@ -1,48 +1,40 @@
 const PORT = process.env.PORT || 5001;
 var express = require('express');
-var app = express();   //wszystkie trzy wymagane dal heroku
+var app = express();
 
 var Twit = require('twit');
 var config = require('./config.js');
-//var fs = require('fs');
 const fetch = require("node-fetch");
 var T = new Twit(config);
 
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`)); //dla heroku
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`)); //for heroku
 
-setInterval(tweeter, 60*29*1000); //60*60 to godzina
+setInterval(tweeter, 60*29*1000); //60*60 = 1 hr
 
 tweeter();
 
 function tweeter() {
-
   fetch('https://api.covid19api.com/summary')
   .then(response => {
     return response.json();
   })
   .then(users => {
-    // console.log(users);
     var json = JSON.stringify(users, null, 2);   //punkt pierwszy przy czytaniu JSON
-    //fs.writeFileSync('tweets.json', json);      //towrzy plik json
 
     var obj = JSON.parse(json);     //punkt drugi przy czytaniu JSON
     var globalNewConfirmed = obj.Global.NewConfirmed;   //zmienna "b" jako odnośnik do Global -> NewConfirmed w JSON
 
-    //polska
-    var namePoland = obj.Countries[135].Country;
     var laczniePrzypadkow = obj.Countries[135].TotalConfirmed.toLocaleString();
     var nowychPrzypadkow = obj.Countries[135].NewConfirmed.toLocaleString();
     var lacznieZmarlo = obj.Countries[135].TotalDeaths.toLocaleString();
     var lacznieWyzdrowialo = obj.Countries[135].TotalRecovered.toLocaleString();
     var nowychWyzdrowialo = obj.Countries[135].NewRecovered.toLocaleString();
     var nowychZmarlo = obj.Countries[135].NewDeaths.toLocaleString();
-    var ostatniaAktualizacja = obj.Countries[135].Date;
 
     var dataForTweet = ("Chorych: " + laczniePrzypadkow + " (+ " + nowychPrzypadkow + "). \nWyzdrowialo: " + lacznieWyzdrowialo + " (+ " + nowychWyzdrowialo +"). \nZmarlo: " + lacznieZmarlo + " (+ " + nowychZmarlo + ").\n\n#covid19 #sarscov2");
-    //console.log(dataForTweet);
-
     T.post('statuses/update', { status: dataForTweet }, function(err, data, response) {
-    console.log(data)
+      if (err) throw err;
+      console.log(data);
 })
   });
 
